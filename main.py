26dbd9b371d5bb9e8f48a302cd4039f4066a0c29
Anurudh Mhatre <59391974+raavann@users.noreply.github.com>
2021-10-13 17:35:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,12 +36,64 @@ from cogs import society_members
 # Voting ends + results: \end
 
 
-# Set the command prefix to be '\'
-PREFIX = '\\'
+# Set the default prefix to be '\'
+DEFAULT_PREFIX = '\\'
 
-# Create the bot and specify to only look for messages starting with the PREFIX
-bot = commands.Bot(command_prefix=PREFIX)
+def get_prefix(bot_obj, message) -> str:
+    # a custom prefix can be retrieved with the guild's ID
+    try:
+        with open("prefixes.json", 'r') as f:
+            prefixes = json.load(f)
 
+        return prefixes[str(message.guild.id)]
+
+    except AttributeError:  # triggered when command is invoked in dms
+        return DEFAULT_PREFIX
+
+    except KeyError:  # triggered when the server prefix has never been changed
+        return DEFAULT_PREFIX
+
+client = commands.Bot(command_prefix=get_prefix, intents=intents)
+
+
+@client.event
+async def on_guild_remove(guild):
+    try:
+        with open("prefixes.json", 'r') as f:
+            prefixes = json.load(f)
+
+        prefixes.pop(str(guild.id))
+
+        with open("prefixes.json", 'w') as f:
+            json.dump(prefixes, f, indent=2)
+    except KeyError:  # the server never changed the default prefix
+        pass
+
+@client.command(aliases=["PREFIX", "Prefix", "pREFIX"])
+@has_permissions(administrator=True)
+async def prefix(ctx, new_prefix: str):
+
+    if ctx.guild is None:
+        await ctx.reply("**You cannot change the prefix outside of a server!**")
+        return
+
+    with open("prefixes.json", 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes[str(ctx.guild.id)] = new_prefix
+
+    with open("prefixes.json", 'w') as f:
+        json.dump(prefixes, f, indent=2)
+
+    await ctx.send(f"**Prefix changed to {new_prefix}**")
+
+@prefix.error
+async def prefix_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.reply("**Incorrect usage!\n"
+                        f"Example: {get_prefix(client, ctx)}prefix .**")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.reply("**You do not have the permission to change the server prefix!**")
 
 @bot.event
 async def on_ready():
